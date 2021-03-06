a4cdf3a6f6e45564ef0ac0c8a6314f891ed5e8f9
neo qwert <ganeshanbvn@gmail.com>
2021-03-06 21:26:01 +0530

    first commit

---
+++ twocam/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from twocam.camera import IPWebCam, VideoCamera
+import multiprocessing
+
+# Create your views here.
+def index(request):
+        return render(request,'index.html')
+    
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+  
+def webcam_feed(request):
+	return StreamingHttpResponse(gen(IPWebCam()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+  
+def video_feed(request):
+	return StreamingHttpResponse(gen(VideoCamera()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+ 
